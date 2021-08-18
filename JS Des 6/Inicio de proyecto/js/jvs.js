181999e67e311a32a77479c5502ef263df8bbff0
Frs.Vall <77133137+frsvaleria@users.noreply.github.com>
2021-08-18 00:08:08 -0300

    Array de compra de viajes con Js

---
+++ JS Des 6/Inicio de proyecto/js/jvs.js
@@ -0,0 +1,76 @@
+let vender = "vender";
+
+
+alert("Bienvenido")
+
+
+const nombre = prompt("Ingrese nombre de usuario");
+alert('Hola' + nombre);
+
+const contraseña = prompt("Ingrese su contraseña ")
+alert('Contraseña validada')
+
+
+const ciudad = prompt("ingrese ciudad de partida")
+alert('Destino de partida chequeado')
+
+alert(nombre + 'parte desde' + ciudad)
+
+let pasaje1 = {
+    destino='tolhuin',
+    transporte='bus',
+    calidad='economico',
+}
+
+let pasaje2 = {
+    destino= 'san sebastian',
+    transporte='barco',
+    calidad='claseA'
+}
+
+
+let pasaje3 = {
+    destino= 'cerro sombrero',
+    transporte= 'avion',
+    calidad= 'turista',
+}
+
+let pasaje4 = {
+    destino= 'Rio grande',
+    transporte='avion',
+    calidad= 'claseA',
+}
+
+
+class Pasaje {
+    constructor(destino, transporte, calidad) {
+        this.destino = destino;
+        this.transporte = transporte;
+        this.calidad = calidad;
+        this.vendido = false;
+    }
+}
+
+
+
+
+
+
+confirmaCantidad()
+this.pasaje = this.pasaje * 1;
+
+
+
+
+const pasajes = [];
+pasaje1.push(new pasaje("tolhuin", "bus", "economico"));
+pasaje2.push(new pasaje("san sebastian", "barco", "claseA"));
+pasaje3.push(new pasaje("cerro sombrero", "avion", "turista"));
+pasaje4.push(new pasaje("Rio grande", "avion", "claseA"));
+
+
+for (const pasaje of pasajes)
+
+    pasaje.confirmaPaso2();
+
+console.log(producto1);
