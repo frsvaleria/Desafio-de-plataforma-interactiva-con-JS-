3f2e3b70c310685ce5a4f872120ecfa792eb885f
friasvall <frias.vall@gmail.com>
2021-09-21 21:09:56 -0300

    nuevo comit

---
+++ Js 07 viajes/js/productos.js
@@ -0,0 +1,121 @@
+let productos = [
+    {
+        id: 1,
+        destino: "cerro sombrero",
+        calidad: "turista",
+        transporte: "bus",
+        precio: "14590"
+    },
+    {
+        "id": 2,
+        destino: "Tolhuin",
+        calidad: "turista",
+        transporte: "bus",
+        precio: "18000"
+    },
+    {
+        id: 3,
+        destino: "san sebastian",
+        calidad: "primeraClase",
+        transporte: "bus",
+        precio: "18600"
+    },
+    {
+        id: 4,
+        destino: "rio grande",
+        calidad: "turista",
+        transporte: "bus",
+        precio: "16590"
+    },
+    {
+        id: 5,
+        destino: "guayana francesa",
+        calidad: "turista",
+        transporte: "barco",
+        precio: "28490"
+    },
+    {
+        id: 6,
+        destino: "mexico",
+        calidad: "turista",
+        transporte: "barco",
+        precio: "22590"
+    },
+    {
+        id: 7,
+        destino: "porto do sau paulo",
+        calidad: "primeraClase",
+        transporte: "barco",
+        precio: "19250"
+    },
+    {
+        id: 8,
+        destino: "isla de pascua",
+        calidad: "turista",
+        transporte: "barco",
+        precio: "24590"
+    },
+    {
+        id: 9,
+        destino: "Mexico - Peru - Argentina",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "12890"
+    },
+    {
+        id: 10,
+        destino: "Argentina - sin escala -- Canada",
+        calidad: "primeraClase",
+        transporte: "avion",
+        precio: "16580"
+    },
+    {
+        id: 11,
+        destino: "Australia - España - Canada",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "18650"
+    },
+    {
+        id: 12,
+        destino: "Rusia - Portugal - Uruguay",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "18000"
+    },
+    {
+        id: 13,
+        destino: "Buenos Aires -España - Italia",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "17689"
+    },
+    {
+        id: 14,
+        destino: "Mexico - sin escala - Rusia",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "15650"
+    },
+    {
+        id: 15,
+        destino: "Argentina - sin escala -- España",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "16500"
+    },
+    {
+        id: 16,
+        destino: "Marruecos - sin escala - Canada",
+        calidad: "primeraClase",
+        transporte: "avion",
+        precio: "15000"
+    },
+    {
+        id: 17,
+        destino: "Francia - Portugal - Chile",
+        calidad: "turista",
+        transporte: "avion",
+        precio: "18900"
+    }
+]
